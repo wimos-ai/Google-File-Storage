bed736ee45debc0ab14340c8e6299c1f8f6020dc
wimos-ai <willmoiser@gmail.com>
2024-01-02 22:12:48 -0600

    Added better error handling

--- src/file_enc.rs
+++ src/file_enc.rs
@@ -2,41 +2,63 @@ use aes_gcm::{aead::{Aead, AeadCore, KeyInit, OsRng}, Aes256Gcm, Nonce, Key};
 
 use std::fs::File;
 use std::io::{Read, Write};
-use std::path::Path;
+use std::path::{Path};
 
 const NONCE_SIZE: usize = 12;
 
 /// TODO: Go in blocks
 
-pub fn encrypt_file(input_file: &Path, output_file: &Path, key: &Key<Aes256Gcm>) {
-    let mut file_in = File::open(input_file).expect("Failed to open input file");
-    let mut file_out = File::create(output_file).expect("Failed to open output file");
+#[derive(Debug)]
+pub enum FileEncError{
+    IOError(std::io::Error),
+    EncryptionError(aes_gcm::Error)
+}
+
+impl From<std::io::Error> for FileEncError {
+    fn from(e: std::io::Error) -> Self {
+        FileEncError::IOError(e)
+    }
+}
+
+impl From<aes_gcm::Error> for FileEncError {
+    fn from(e: aes_gcm::Error) -> Self {
+        FileEncError::EncryptionError(e)
+    }
+}
+
+
+
+pub fn encrypt_file(input_file: &Path, output_file: &Path, key: &Key<Aes256Gcm>) -> Result<(), FileEncError> {
+    let file_in = File::open(input_file)?;
+
+    let mut file_out = File::create(output_file)?;
 
     let nonce = Aes256Gcm::generate_nonce(&mut OsRng); // 96-bits; unique per message
-    let mut encryptor = Aes256Gcm::new(&key);
+    let encryptor = Aes256Gcm::new(&key);
 
-    let mut file_bytes = vec![0;file_in.metadata().unwrap().len() as usize];
-    file_in.read(file_bytes.as_mut_slice()).expect("Could not read file bytes");
+    let file_bytes = vec![0; file_in.metadata()?.len() as usize];
 
-    let enc_bytes = encryptor.encrypt(&nonce,file_bytes.as_ref()).expect("Could not encrypt file");
+    let enc_bytes = encryptor.encrypt(&nonce, file_bytes.as_ref())?;
 
     file_out.write(&nonce).expect("Could not write Nonce");
     file_out.write(&enc_bytes).expect("Could not write enc bytes");
 
+    Ok(())
 }
 
-pub fn decrypt_file(input_file: &Path, output_file: &Path, key: &Key<Aes256Gcm>) {
-    let mut file_in = File::open(input_file).expect("Failed to open input file");
-    let mut file_out = File::create(output_file).expect("Failed to open output file");
+pub fn decrypt_file(input_file: &Path, output_file: &Path, key: &Key<Aes256Gcm>) -> Result<(), FileEncError>{
+    let mut file_in = File::open(input_file)?;
+    let mut file_out = File::create(output_file)?;
 
-    let mut file_bytes = vec![0;file_in.metadata().unwrap().len() as usize];
-    file_in.read(file_bytes.as_mut_slice()).expect("Could not read file bytes");
+    let mut file_bytes = vec![0; file_in.metadata()?.len() as usize];
+    file_in.read(file_bytes.as_mut_slice())?;
 
     let nonce = Nonce::from_slice(&file_bytes[0..NONCE_SIZE]);
-    let mut encryptor = Aes256Gcm::new(&key);
+    let encryptor = Aes256Gcm::new(&key);
 
-    let denc_bytes = encryptor.decrypt(&nonce,&file_bytes[NONCE_SIZE..]).expect("Could not encrypt file");
+    let decrypted_bytes = encryptor.decrypt(&nonce, &file_bytes[NONCE_SIZE..])?;
 
-    file_out.write(&denc_bytes).expect("Could not write enc bytes");
+    file_out.write(&decrypted_bytes)?;
 
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,14 @@ use std::path::Path;
 use std::fs::File;
 use std::io::{BufReader, Read};
 
-use aes_gcm::{
-    Aes256Gcm, Key, // Or `Aes128Gcm`
-};
+use aes_gcm::{Aes256Gcm, Key};
 
 fn is_same_file(file1: &Path, file2: &Path) -> Result<bool, std::io::Error> {
     let f1 = File::open(file1)?;
     let f2 = File::open(file2)?;
 
     // Check if file sizes are different
-    if f1.metadata().unwrap().len() != f2.metadata().unwrap().len() {
+    if f1.metadata()?.len() != f2.metadata()?.len() {
         return Ok(false);
     }
 
@@ -24,7 +22,7 @@ fn is_same_file(file1: &Path, file2: &Path) -> Result<bool, std::io::Error> {
 
     // Do a byte to byte comparison of the two files
     for (b1, b2) in f1.bytes().zip(f2.bytes()) {
-        if b1.unwrap() != b2.unwrap() {
+        if b1? != b2? {
             return Ok(false);
         }
     }
@@ -40,8 +38,8 @@ fn main() {
     let enc_file = Path::new("C:\\Users\\willm\\RustroverProjects\\Google-File-Storage\\tmp.bin.enc");
     let d_enc_file = Path::new("C:\\Users\\willm\\RustroverProjects\\Google-File-Storage\\tmp.denc.bin");
 
-    file_enc::encrypt_file(input_file, enc_file, &k);
-    file_enc::decrypt_file(enc_file, d_enc_file, &k);
+    file_enc::encrypt_file(input_file, enc_file, &k).expect("Could not encrypt file");
+    file_enc::decrypt_file(enc_file, d_enc_file, &k).expect("Could not decrypt file");
 
     assert!(is_same_file(input_file, d_enc_file).unwrap(), "Files Were not the same :(");
 }
